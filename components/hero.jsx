f0927d654b02c07f4c317145c5c036189533d3ff
Eshit Gandhi <114164248+EshitGandhi@users.noreply.github.com>
2025-11-26 01:41:56 +0530

    Remove redundant Link component in hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -42,11 +42,11 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link>
+      
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
-          </Link>
+         
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
